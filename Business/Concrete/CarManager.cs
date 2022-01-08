f028fffd8aa2d935ca3dac1a5aee3a3f40cc76d4
EmirhanAltuntas <altuntasemirhan99@gmail.com>
2022-01-08 19:51:19 +0300

    12. gün AOP Desteği Eklendi-AspectValidation Eklendi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -22,7 +23,7 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
-        public IDataResult<List<Car>>  GetAll()
+        public IDataResult<List<Car>> GetAll()
         {
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarsListed);
         }
@@ -36,11 +37,11 @@ namespace Business.Concrete
         {
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.ColorId == id));
         }
+
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            
-            ValidationTool.Validate(new CarValidator(), car);
-
+ 
             _carDal.Add(car);
 
             return new SuccessResult(Messages.CarAddedMessage);
